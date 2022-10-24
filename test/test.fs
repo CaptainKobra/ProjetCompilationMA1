73bccd0a027047138afca028e169edc966abfb7c
Maxime Pellichero <96079270+CaptainKobra@users.noreply.github.com>
2022-10-24 20:00:13 +0200

    Add files via upload

---
+++ test/test.fs
@@ -0,0 +1,14 @@
+BEGIN Greaterthan
+
+%% %%
+
+READ(number3) ,              :: Read a number from user input
+res := number3 / 2 ,
+
+IF (res < 1 ) THEN
+	
+	PRINT(1),
+ELSE
+	PRINT(0),
+ENDIF ,
+END
