1f0ddbd09104973c8dbcbf606d8bde1a1a289715
RedhwaneBS <99534301+RedhwaneBS@users.noreply.github.com>
2022-10-24 17:34:22 +0200

    Add files via upload

---
+++ ProjetCompilationMA1/test/test.fs
@@ -0,0 +1,15 @@
+BEGIN Greaterthan
+
+%% %%
+
+READ(number3) ,              :: Read a number from user input
+res := number3 / 2 ,:: Read a number from user input
+
+IF (res < 1 ) THEN
+	
+	PRINT(1),
+ELSE
+	PRINT(0),
+ENDIF ,
+serarien := "/0",
+END
